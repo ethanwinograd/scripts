6a0ffb5e4cb4a361ba9221300a2bc8e7042070a7
Ethan Winograd <ethanwinograd@users.noreply.github.com>
2014-08-06 16:00:04 -0400

    Update vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -6,6 +6,10 @@ sudo apt-get install curl
 #set up pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle; curl -LSso ~/.vim/autoload/pathogen.vim     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
+echo "creating vim backup locations.."
+mkdir ~/.vim
+mkdir ~/.vim/backup
+
 sudo  apt-get install vim-gui-common
 sudo apt-get install vim-runtime
 
