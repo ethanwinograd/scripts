497811c63174670cee1fdd5157adb8370264b9c0
Ethan Winograd <winograde@upmc.edu>
2014-09-22 09:22:47 -0400

    added readme,openssl.cnf, and edited script to create testca subdir.

--- certs/make_all_certs.sh
+++ certs/make_all_certs.sh
@@ -2,13 +2,22 @@
 
 array=(server1.somedomain.com, server2.somedomain.com)
 
+mkdir testca
+
+mv openssl.cnf testca
+
 cd testca
+mkdir certs private
+chmod 700 private
+echo 01 > serial
+touch index.txt
 
 rm serial
 
 echo 01 > serial
 
 touch index.txt
+
 
 openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 3650 \
 -out cacert.pem -outform PEM -subj /CN=MyTestCA/ -nodes
