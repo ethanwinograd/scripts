295f5ea7b206828a75cdbd830e77af52b732d777
Ethan Winograd <winograde@upmc.edu>
2014-10-08 10:07:04 -0400

    added tmp file to setup

--- vim_setup.sh
+++ vim_setup.sh
@@ -4,11 +4,12 @@
 sudo apt-get install curl
 
 #set up pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle; curl -LSso ~/.vim/autoload/pathogen.vim     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/plugin; curl -LSso ~/.vim/autoload/pathogen.vim     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 echo "creating vim backup locations.."
 mkdir ~/.vim
 mkdir ~/.vim/backup
+mkdir ~/.vim/tmp
 
 sudo  apt-get install vim-gui-common
 sudo apt-get install vim-runtime
